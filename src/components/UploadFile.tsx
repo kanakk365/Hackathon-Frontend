7ea379a2e15724187040ffde2cf9f09e2a73793a
Kanak <kanakkumarmahala000@gmail.com>
2025-01-09 00:51:02 +0530

    remove some more imoprts

--- src/components/UploadFile.tsx
+++ src/components/UploadFile.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import { useState } from "react";
 import { FileUpload } from "@/components/ui/file-upload";
 import axios from "axios";
 import { Button } from "./ui/button";
